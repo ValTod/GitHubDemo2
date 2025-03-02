2f20481d335f64d2861c7ae543b193a722402a91
ValTod <33367731+ValTod@users.noreply.github.com>
2025-03-02 10:35:38 +0200

    Added comments

---
+++ GitExercise/OptionsManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace GitExercise
+{
+    public static class OptionsManager
+    {
+        // Added OptionsList
+        public static string[] OptionsList = {
+            "a - Add",
+            "s - Subtract",
+            "m - Multiply"
+        };
+
+        public static void Add(double a, double b)      // Added Add method
+        {
+            Console.WriteLine($"{a} + {b} = {a + b}");
+        }
+
+        public static void Multiply(double a, double b)    // Added Multiply method
+        {
+            Console.WriteLine($"{a} x {b} = {a * b}");
+        }
+
+        public static void Subtract(double a, double b) // Added Subtract method
+        {
+            Console.WriteLine($"{a} - {b} = {a - b}");
+        }
+    }
+}
